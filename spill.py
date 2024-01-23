ed22144febec33a44c21d848a5d8799273c225e6
Beate <beate.gulhaugen@gmail.com>
2024-01-23 11:28:10 +0100

    shell

--- spill.py
+++ spill.py
@@ -1,6 +1,17 @@
-import pygame
+import pygame 
+pygame.init()
+skjerm= pygame.display.set_mode((500,500))
+klokke=pygame.time.Clock()
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+    klokke.tick(30)
+    skjerm.fill("white")
+    
+    pygame.display.flip()
 
 
-pygame.init() #jkwjwkjrk
-print("hello")
-print ("test")
+
+pygame.quit()
