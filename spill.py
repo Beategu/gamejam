2b120c9b34f69ef0370f2306d55005837b7e5aad
Beate <beate.gulhaugen@gmail.com>
2024-01-30 12:09:09 +0100

    hj

--- spill.py
+++ spill.py
@@ -28,7 +28,7 @@ class Packman(Felles):
             if self.status:
 
                 self.x-=self.fart
-                print ("oink")
+                
         elif tast[pygame.K_d]:
             self.rekttest=pygame.Rect((self.x+self.fart,self.y),(self.strx,self.stry))
             self.rekt=pygame.Rect((self.x,self.y),(self.strx,self.stry))
@@ -40,7 +40,7 @@ class Packman(Felles):
             if self.status:
 
                 self.x+=self.fart
-                print ("oink")
+                
         elif tast[pygame.K_w]:
             self.rekttest=pygame.Rect((self.x,self.y-self.fart),(self.strx,self.stry))
             self.rekt=pygame.Rect((self.x,self.y),(self.strx,self.stry))
@@ -52,7 +52,7 @@ class Packman(Felles):
             if self.status:
 
                 self.y-=self.fart
-                print ("oink")
+                
         elif tast[pygame.K_s]:
             self.rekttest=pygame.Rect((self.x,self.y+self.fart),(self.strx,self.stry))
             self.rekt=pygame.Rect((self.x,self.y),(self.strx,self.stry))
@@ -64,23 +64,89 @@ class Packman(Felles):
             if self.status:
 
                 self.y+=self.fart
-                print ("oink")
+                
+    def livEllerDød(self):
+        status=True
+        for x in fiender:
+            if pygame.Rect.colliderect(x.rekt,self.rekt):
+                status=False
+        return status
+
     
 
 class Labirynt(Felles):
     def __init__(self, x,y,strx,stry):
         super().__init__(x,y,"purple",strx,stry)
 
+class Fiende (Felles):
+    def __init__(self, x, y, akse, xstr, ystr, fart):
+        self.x =x
+        self.y=y
+        self.akse=akse
+        self.strx=xstr
+        self.stry=ystr
+        super().__init__(self.x, self.y, "red", self.strx, self.stry)
+        self.fart=fart
+    def bevege(self):
+        if self.akse=="y":
+            for x in laberyntlist:
+                if pygame.Rect.colliderect(x.rekt, self.rekt):
+                    self.fart=self.fart*(-1)
+            self.y+=self.fart
+        if self.akse=="x":
+            for x in laberyntlist:
+                if pygame.Rect.colliderect(x.rekt, self.rekt):
+                    self.fart=self.fart*(-1)
+            self.x+=self.fart
+    
+
+
+class Poeng(Felles):
+    def __init__(self):
+        self.x= random.randint(0,skjerm.get_width())
+        self.y=random.randint(0,skjerm.get_height())
+        self.farge="green"
+        self.strx= random.randint(3,int(skjerm.get_width()/60))
+        self.stry= random.randint(3,int(skjerm.get_height()/60))
+        super().__init__( self.x, self.y, self.farge, self.strx, self.stry)
+        
+    def oppdater(self):
+
+        for x in laberyntlist:
+            if pygame.Rect.colliderect(self.rekt, x.rekt):
+                self.x= random.randint(0,skjerm.get_width())
+                self.y=random.randint(0,skjerm.get_height())
+                self.farge="green"
+                self.strx= random.randint(3,int(skjerm.get_width()/50))
+                self.stry= random.randint(3,int(skjerm.get_height()/50))
+                break
+        if pygame.Rect.colliderect(self.rekt, pack.rekt):
+                self.x= random.randint(0,skjerm.get_width())
+                self.y=random.randint(0,skjerm.get_height())
+                self.farge="green"
+                self.strx= random.randint(3,int(skjerm.get_width()))
+                self.stry= random.randint(3,int(skjerm.get_height()))
+                return True
+        else:
+            return False
 
 
 
 
+
+
+import random
 import pygame 
+
 pygame.init()
+pygame.font.init()
 skjerm= pygame.display.set_mode((500,500))
+font= pygame.font.SysFont("Arial", int(skjerm.get_height()/20))
+font2= pygame.font.SysFont("Arial", int(skjerm.get_height()/4))
+
 klokke=pygame.time.Clock()
 running=True
-test=Packman()
+pack=Packman()
 #vegger
 lab1= Labirynt(0,0, skjerm.get_width(), skjerm.get_height()/25)
 lab2= Labirynt(0,0, skjerm.get_width()/25, skjerm.get_height())
@@ -106,17 +172,59 @@ lab11= Labirynt((skjerm.get_width()/5),(skjerm.get_height()/4)*3, skjerm.get_wid
 lab12= Labirynt((skjerm.get_width()/3)*2,(skjerm.get_height()/4)*3, skjerm.get_width()/25, skjerm.get_height()/25*4)#vertikal
 
 laberyntlist=[lab1, lab2, lab3, lab4, lab5,lab6,lab7, lab8, lab9, lab10, lab11, lab12, lab13, lab14, lab15,lab16, lab17, lab18, lab19]
+
+fiende= Fiende(skjerm.get_width()/12*11, skjerm.get_height()/7, "y", skjerm.get_width()/30, skjerm.get_height()/30,3)
+
+fiende2= Fiende(skjerm.get_width()/12*10, skjerm.get_height()/7*6, "y", skjerm.get_width()/30, skjerm.get_height()/30,5)
+fiende3= Fiende(skjerm.get_width()/12, skjerm.get_height()/4, "x", skjerm.get_width()/30, skjerm.get_height()/30,6)
+fiende4= Fiende(skjerm.get_width()/12, skjerm.get_height()/12*11, "x", skjerm.get_width()/30, skjerm.get_height()/30,2)
+fiende5= Fiende(skjerm.get_width()/7, skjerm.get_height()/10*7, "x", skjerm.get_width()/30, skjerm.get_height()/30,3)
+fiende6= Fiende(skjerm.get_width()/12*10, skjerm.get_height()/7*6, "y", skjerm.get_width()/30, skjerm.get_height()/30,5)
+fiende7= Fiende(skjerm.get_width()/12*9, skjerm.get_height()/7*6, "y", skjerm.get_width()/30, skjerm.get_height()/30,4)
+fiende8= Fiende(skjerm.get_width()/2, skjerm.get_height()/12*5, "x", skjerm.get_width()/30, skjerm.get_height()/30,7)
+
+fiender=[fiende,fiende2, fiende3, fiende4, fiende5, fiende6, fiende7, fiende8]
+
+poeng=Poeng()
+
+start=True
+igang=False
+slutt=False
+sum=0
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
     klokke.tick(30)
     skjerm.fill("black")
-    for x in laberyntlist:
-        x.tegn()
-    test.oppdater()
-    test.tegn()
-    
+    if start:
+        tast= pygame.key.get_pressed()
+        if tast[pygame.K_SPACE]:
+            start=False
+            igang=True
+        tekst=font.render("Klikk space for å starte", True, "White")
+        skjerm.blit(tekst, (skjerm.get_width()/3, skjerm.get_height()/2))
+    elif igang:
+        for x in laberyntlist:
+            x.tegn()
+        pack.oppdater()
+        pack.tegn()
+        for x in fiender:
+            x.bevege()
+            x.tegn()
+        poeng.tegn()
+        if poeng.oppdater():
+            sum+=1
+        tekst2= font.render(str(sum), True, "White")
+        skjerm.blit(tekst2, (skjerm.get_width()/40, skjerm.get_height()/60))
+
+        if pack.livEllerDød()==False:
+            igang=False
+            slutt=True
+        
+    if slutt:
+        tekst3= font.render((f"du fikk {sum} poeng"), True, "White")
+        skjerm.blit(tekst3, (skjerm.get_width()/3, skjerm.get_height()/2))
     pygame.display.flip()
 
 
